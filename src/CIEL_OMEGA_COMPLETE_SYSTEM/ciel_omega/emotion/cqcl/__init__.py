85b6b642f69518d756edb916b55fb36163dd1a7e
Adrian Lipa <adrianlipa90@gmail.com>
2026-08-27 17:27:13 +0100

    Export CQCL vNext living compiler additively

--- src/CIEL_OMEGA_COMPLETE_SYSTEM/ciel_omega/emotion/cqcl/__init__.py
+++ src/CIEL_OMEGA_COMPLETE_SYSTEM/ciel_omega/emotion/cqcl/__init__.py
@@ -3,5 +3,14 @@
 from emotion.cqcl.cqcl_program import CQCL_Program
 from emotion.cqcl.quantum_engine import CIEL_Quantum_Engine
 from emotion.cqcl.emotional_collatz import EmotionalCollatzEngine
+from emotion.cqcl.living_program_v01 import CQCL_Living_Program
+from emotion.cqcl.living_compiler_v01 import CQCLLivingError, compile_living_program
 
-__all__ = ["CQCL_Program", "CIEL_Quantum_Engine", "EmotionalCollatzEngine"]
+__all__ = [
+    "CQCL_Program",
+    "CIEL_Quantum_Engine",
+    "EmotionalCollatzEngine",
+    "CQCL_Living_Program",
+    "CQCLLivingError",
+    "compile_living_program",
+]
